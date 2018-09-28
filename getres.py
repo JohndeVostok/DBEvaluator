d1086ffd79ce20dc308542f216001683b4f227f6
mazx <dyxdy@live.com>
2018-09-29 02:53:56 +0800

    sb

---
+++ getres.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+	for i in range(13):
+		for j in range(8):
+			s = "res/testres" + str(2 ** j) + "c1s" + str(i) + "l.txt"
+			with open(s, "r") as f:
+				data = f.readlines();
+			print(data[2][data[2].index(":")+2:-1] + "," + data[23][data[23].index(":")+2:-1])
+		print("\n")
