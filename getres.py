d1f1fc304e7f4853f8c10dd2742e7a53e3cb5f05
mzx <mazx15@mails.tsinghua.edu.cn>
2018-09-30 15:57:53 +0800

    fuck

--- getres.py
+++ getres.py
@@ -1,7 +1,7 @@
 if __name__ == "__main__":
 	for i in range(13):
-		for j in range(8):
-			s = "res/testres" + str(2 ** j) + "c1s" + str(i) + "l.txt"
+		for j in range(7):
+			s = "res/testres" + str(2 ** j) + "c3s" + str(i) + "l.txt"
 			with open(s, "r") as f:
 				data = f.readlines();
 			print(data[2][data[2].index(":")+2:-1] + "," + data[23][data[23].index(":")+2:-1])
